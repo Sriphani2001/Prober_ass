1fe376ea84e7f88f52c46b33cf966c210e3c6e1d
Sri Phani Deverakonda <58811487+Sriphani2001@users.noreply.github.com>
2024-06-10 22:20:43 +0545

    Update ass3_nso.py

--- ass3_nso.py
+++ ass3_nso.py
@@ -3,113 +3,124 @@ import easysnmp
 from easysnmp import Session
 import sys, time, math
 
-Agent_name = sys.argv[1]
-agent_iput = Agent_name.split(':')
-Agent_Ipadd = agent_iput[0]
-Agent_Port = agent_iput[1]
-Community_of_agent = agent_iput[2]
-SampleFreq = float(sys.argv[2])
-Counts_sample = int(sys.argv[3])
-SamplingTime = 1 / SampleFreq
-ObjIDs = []
-ObjectIdentifier1 = []
-ObjectIdentifier2 = []
-timer = ""
+# Extract agent details and command-line arguments
+agent_info = sys.argv[1]
+agent_details = agent_info.split(':')
+agent_ip = agent_details[0]
+agent_port = agent_details[1]
+agent_community = agent_details[2]
+sampling_frequency = float(sys.argv[2])
+sample_count = int(sys.argv[3])
+sampling_interval = 1 / sampling_frequency
+oids = []
 
-for cha in range(4, len(sys.argv)):
-    ObjIDs.append(sys.argv[cha])
+# Collect OIDs from command-line arguments
+for i in range(4, len(sys.argv)):
+    oids.append(sys.argv[i])
 
-ObjIDs.insert(0, '1.3.6.1.2.1.1.3.0')
+# Add sysUpTime OID to the list of OIDs to be monitored
+oids.insert(0, '1.3.6.1.2.1.1.3.0')
 
-session = Session(hostname=Agent_Ipadd, remote_port=Agent_Port,
-                  community=Community_of_agent, version=2, timeout=1, retries=3)
+# Initialize SNMP session
+session = Session(hostname=agent_ip, remote_port=agent_port,
+                  community=agent_community, version=2, timeout=1, retries=3)
 
-def chadu():
-    global ObjectIdentifier1, new_time, timer2
-    oput = session.get(ObjIDs)
-    TimerSys = int(oput[0].value) / 100
-    ObjectIdentifier2 = []
+# Function to fetch SNMP data and calculate rates
+def fetch_snmp_data():
+    global previous_values, previous_time, last_printed_time
+    response = session.get(oids)
+    current_time = int(response[0].value) / 100
+    current_values = []
 
-    for t in range(0, len(oput)):
-        print("oput value{}".format(oput[t].value))
+    # Print current OID values for debugging
+    for item in response:
+        print(f"SNMP Response value: {item.value}")
 
-    if int(TimerSys) > 2**32 or int(TimerSys) <= 0:
+    # Check if the system has restarted
+    if int(current_time) > 2**32 or int(current_time) <= 0:
         print("The system just restarted.")
 
-    for du in range(1, len(oput)):
-        if oput[du].value != 'NOSUCHOBJECT' and oput[du].value != 'NOSUCHINSTANCE':
-            if oput[du].snmp_type in ['COUNTER64', 'GAUGE', 'COUNTER']:
-                ObjectIdentifier2.append(int(oput[du].value))
+    # Process OID values from the response
+    for i in range(1, len(response)):
+        if response[i].value != 'NOSUCHOBJECT' and response[i].value != 'NOSUCHINSTANCE':
+            if response[i].snmp_type in ['COUNTER64', 'GAUGE', 'COUNTER']:
+                current_values.append(int(response[i].value))
             else:
-                ObjectIdentifier2.append(oput[du].value)
+                current_values.append(response[i].value)
 
-            if count != 0 and len(ObjectIdentifier1) > 0:
-                if TimerSys > new_time:
-                    if oput[du].snmp_type in ['COUNTER', 'COUNTER32', 'COUNTER64']:
-                        OIDdiff = int(ObjectIdentifier2[du - 1]) - int(ObjectIdentifier1[du - 1])
-                        diff_time = (TimerSys - new_time)
-                        rate = OIDdiff / diff_time
+            if count != 0 and len(previous_values) > 0:
+                if current_time > previous_time:
+                    if response[i].snmp_type in ['COUNTER', 'COUNTER32', 'COUNTER64']:
+                        oid_difference = int(current_values[i - 1]) - int(previous_values[i - 1])
+                        time_difference = (current_time - previous_time)
+                        rate = oid_difference / time_difference
 
+                        # Handle counter wrap-around
                         if rate < 0:
-                            if oput[du].snmp_type == 'COUNTER32':
-                                OIDdiff = OIDdiff + (2**32)
-                            elif oput[du].snmp_type == 'COUNTER64':
-                                OIDdiff = OIDdiff + (2**64)
-                            rate = OIDdiff / diff_time
+                            if response[i].snmp_type == 'COUNTER32':
+                                oid_difference = oid_difference + (2**32)
+                            elif response[i].snmp_type == 'COUNTER64':
+                                oid_difference = oid_difference + (2**64)
+                            rate = oid_difference / time_difference
 
                         try:
-                            if timer == str(timer2):
+                            if last_printed_time == str(timer2):
                                 print(round(rate), end="|")
                             else:
                                 print(timer2, "|", round(rate), end="|")
-                                timer = str(timer2)
+                                last_printed_time = str(timer2)
                         except:
                             print(timer2, "|", round(rate), end="|")
-                            timer = str(timer2)
-                    elif oput[du].snmp_type == 'GAUGE':
-                        OIDdiff = int(ObjectIdentifier2[du - 1]) - int(ObjectIdentifier1[du - 1])
-                        OIDdiff = "+" + str(OIDdiff)
+                            last_printed_time = str(timer2)
+                    elif response[i].snmp_type == 'GAUGE':
+                        oid_difference = int(current_values[i - 1]) - int(previous_values[i - 1])
+                        oid_difference = "+" + str(oid_difference)
                         try:
-                            if timer == str(timer2):
-                                print(ObjectIdentifier2[len(ObjectIdentifier2) - 1], "(", +OIDdiff, ")", end="|")
+                            if last_printed_time == str(timer2):
+                                print(current_values[-1], "(", oid_difference, ")", end="|")
                             else:
-                                print(timer2, "|", ObjectIdentifier2[len(ObjectIdentifier2) - 1], "(", +OIDdiff, ")", end="|")
-                                timer = str(timer2)
+                                print(timer2, "|", current_values[-1], "(", oid_difference, ")", end="|")
+                                last_printed_time = str(timer2)
                         except:
-                            print(timer2, "|", ObjectIdentifier2[len(ObjectIdentifier2) - 1], "(", +OIDdiff, ")", end="|")
-                            timer = str(timer2)
+                            print(timer2, "|", current_values[-1], "(", oid_difference, ")", end="|")
+                            last_printed_time = str(timer2)
             else:
                 print("This seems like the system was restarted.")
                 break
 
-    ObjectIdentifier1 = ObjectIdentifier2
-    new_time = TimerSys
+    previous_values = current_values
+    previous_time = current_time
 
-if Counts_sample == -1:
+# Initialize variables for the main loop
+previous_values = []
+previous_time = None
+last_printed_time = ""
+
+# Infinite sampling loop if sample_count is -1
+if sample_count == -1:
     count = 0
-    ObjectIdentifier1 = []
     while True:
         timer2 = time.time()
-        chadu()
+        fetch_snmp_data()
         if count != 0:
-            print(end="\n")
-        ResponseTime = time.time()
+            print()
+        response_time = time.time()
         count += 1
-        if SamplingTime >= ResponseTime - timer2:
-            time.sleep(SamplingTime - ResponseTime + timer2)
+        if sampling_interval >= response_time - timer2:
+            time.sleep(sampling_interval - response_time + timer2)
         else:
-            n = math.ceil((ResponseTime - timer2) / SamplingTime)
-            time.sleep((n * SamplingTime) - ResponseTime + timer2)
+            n = math.ceil((response_time - timer2) / sampling_interval)
+            time.sleep((n * sampling_interval) - response_time + timer2)
 else:
-    ObjectIdentifier1 = []
-    for count in range(0, Counts_sample + 1):
+    # Sampling loop for a specified number of samples
+    for count in range(0, sample_count + 1):
         timer2 = time.time()
-        chadu()
+        fetch_snmp_data()
         if count != 0:
-            print(end="\n")
-        ResponseTime = time.time()
-        if SamplingTime >= ResponseTime - timer2:
-            time.sleep(SamplingTime - ResponseTime + timer2)
+            print()
+        response_time = time.time()
+        if sampling_interval >= response_time - timer2:
+            time.sleep(sampling_interval - response_time + timer2)
         else:
-            n = math.ceil((ResponseTime - timer2) / SamplingTime)
-            time.sleep((n * SamplingTime) - ResponseTime + timer2)
+            n = math.ceil((response_time - timer2) / sampling_interval)
+            time.sleep((n * sampling_interval) - response_time + timer2)
